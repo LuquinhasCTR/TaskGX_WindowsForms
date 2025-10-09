844fe44763a78d5c82b750f1f3e4958bf81ac64d
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2025-10-09 12:44:23 +0100

    Adiciona os campos à classe LigacaoDB.

--- TaskGX/LigacaoDB.cs
+++ TaskGX/LigacaoDB.cs
@@ -8,5 +8,30 @@ namespace TaskGX
 {
     public static class LigacaoDB
     {
+        /// <summary>
+        /// Endereço do servidor.
+        /// </summary>
+        public static string Servidor { get; private set; } = "127.0.0.1";
+        /// <summary>
+        /// Nome do utilizador.
+        /// </summary>
+        public static string Utilizador { get; private set; } = "root";
+
+        /// <summary>
+        /// Password do utilizador.
+        /// </summary>
+        public static string Password { get; private set; } = string.Empty;
+        /// <summary>
+        /// Base de dados a utilizar.
+        /// </summary>
+        public static string DB { get; private set; } = "TaskGX_DB";
+        /// <summary>
+        /// Obter a string de ligação (connection string) ao servidor de base de dados.
+        /// </summary>
+        public static string GetConnectionString()
+        {
+            return
+            $"Server={Servidor};database={DB};Uid={Utilizador};Pwd={Password};Charset=utf8mb4;Port = 3306; SslMode = none";
+        }
     }
 }
