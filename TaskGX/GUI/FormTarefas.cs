e4bc6078ba814fb434adbbf2811477175239a4ec
Lucas de Castro Silva <169384752+LuquinhasCTR@users.noreply.github.com>
2025-10-09 14:47:55 +0100

    Adiciona a função CarregarDados.
    Esta função serve para buscar os dados da base de dados e exibí-los na GridViewTarefas, permitindo que o utilizador veja as tarefas que tem para fazer.

--- TaskGX/GUI/FormTarefas.cs
+++ TaskGX/GUI/FormTarefas.cs
@@ -7,6 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Dapper;
+using Microsoft.Win32;
+using MySql.Data.MySqlClient;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ListView;
 
 namespace TaskGX.GUI
 {
@@ -15,6 +19,7 @@ namespace TaskGX.GUI
         public FormTarefas()
         {
             InitializeComponent();
+            CarregarDados();
         }
 
         private void BotaoEditar_Click(object sender, EventArgs e)
@@ -34,6 +39,33 @@ namespace TaskGX.GUI
         }
 
         private void BotaoAdicionar_Click(object sender, EventArgs e)
+        /// <summary>
+        /// O utilizador clicou num item na lista de alunos.
+        /// </summary>
+        private void CarregarDados()
+        {
+            try
+            {
+                // Usa a connection string da tua classe LigacaoDB
+                using (MySqlConnection conexao = new MySqlConnection(LigacaoDB.GetConnectionString()))
+                {
+                    conexao.Open();
+
+                    string query = "SELECT nome AS Nome, descricao AS Descricao, data AS Data FROM tarefa";
+                    using (MySqlDataAdapter adaptador = new MySqlDataAdapter(query, conexao))
+                    {
+                        DataTable tabela = new DataTable();
+                        adaptador.Fill(tabela);
+                        GridViewTarefa.DataSource = tabela;
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Erro ao carregar dados: " + ex.Message, "Erro", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+        private void BindAluno(int v) => throw new NotImplementedException();
         {
 
         }
